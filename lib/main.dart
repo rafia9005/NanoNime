d3224b8e21e5ddabc66beb01dfa23880e596e80b
rafia9005 <rafia9005@gmail.com>
2025-12-20 03:16:39 +0700

    feat: integrate bottom navigation with home, search, and settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nanonime/screen/splash.dart';
 import 'package:nanonime/styles/colors.dart';
 // endpoint
-import 'package:nanonime/screen/main.dart';
+import 'package:nanonime/screen/navigation.dart';
 import 'package:nanonime/screen/auth/login.dart';
 import 'package:nanonime/screen/auth/register.dart';
+import 'package:nanonime/screen/anime/anime_detail.dart';
+import 'package:nanonime/screen/episode/episode_watch.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
   runApp(const Runner());
 }
 
@@ -19,6 +27,7 @@ class Runner extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nanonime',
+      scrollBehavior: ScrollBehavior(),
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
@@ -42,10 +51,41 @@ class Runner extends StatelessWidget {
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => const SplashScreen(),
-        "/": (context) => const MainScreen(),
+        "/": (context) => const NavigationWrapper(),
         "/login": (context) => const AuthLoginScreen(),
         "/register": (context) => const AuthRegisterScreen(),
       },
+      onGenerateRoute: (settings) {
+        final uri = Uri.parse(settings.name ?? "");
+        if (uri.pathSegments.length == 2 && uri.pathSegments[0] == 'anime') {
+          final id = uri.pathSegments[1];
+          return MaterialPageRoute(
+            builder: (context) => AnimeDetailScreen(id: id),
+            settings: settings,
+          );
+        }
+
+        if (uri.pathSegments.length == 2 && uri.pathSegments[0] == 'episode') {
+          final id = uri.pathSegments[1];
+          return MaterialPageRoute(
+            builder: (context) => EpisodeWatchScreen(episodeId: id),
+            settings: settings,
+          );
+        }
+
+        return null;
+      },
     );
+  }
+}
+
+class ScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Widget buildScrollBar(
+    BuildContext context,
+    Widget child,
+    Scrollable details,
+  ) {
+    return child;
   }
 }
